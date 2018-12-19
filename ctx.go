0c5e98bbcab6e2b07f438dfd5b0f1daee0f27c12
Harish Murthy <harish.murthy@qbrics.com>
2018-12-19 11:15:06 +0530

    revert log change

--- ctx.go
+++ ctx.go
@@ -18,7 +18,6 @@ package openssl
 import "C"
 
 import (
-	"log"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -27,12 +26,13 @@ import (
 	"sync"
 	"time"
 	"unsafe"
+	"github.com/spacemonkeygo/spacelog"
 )
 
 var (
 	ssl_ctx_idx = C.X_SSL_CTX_new_index()
 
-	logger = log.Logger
+	logger = spacelog.GetLogger()
 )
 
 type Ctx struct {
--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/vharish836/openssl
+
+require (
+	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572
+	golang.org/x/sys v0.0.0-20181218192612-074acd46bca6 // indirect
+)
